fcb9f5d575739f721fa36aaa486e4f1ebf8b28dd
silas martins <silas07official@gmail.com>
2022-08-19 08:18:21 +0100

    generated table content from an array of objects

---
+++ js/orders.js
@@ -0,0 +1,32 @@
+const orders = [
+  {
+    productName: "2017 Macbook air",
+    productNumber: 1233,
+    status: "due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Birdeye Drone 4K",
+    productNumber: 4533,
+    status: "Paid",
+    shipping: "Shipped",
+  },
+  {
+    productName: "1TB SSD",
+    productNumber: 1533,
+    status: "Failed",
+    shipping: "Declined",
+  },
+  {
+    productName: "iMac 2017 Series",
+    productNumber: 3233,
+    status: "Invoiced",
+    shipping: "Pending",
+  },
+  {
+    productName: "Samsung Galaxy Tab",
+    productNumber: 9233,
+    status: "paid",
+    shipping: "Shipped",
+  },
+];
